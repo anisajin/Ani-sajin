0eb96eb6295724b9e913a1e47939bc2e47d9c9b8
Ani Sajin <ani.sajin@timesworld.com>
2025-08-27 14:37:31 +0530

    Updated project files

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// numver counter
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
+
+
+// for AOS Animation
+
+   AOS.init();
